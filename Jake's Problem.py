ebb15ebc13967d53d112903dd7c8d767b44a4f14
Andrew Walz <83365991+heyguyswalzhere@users.noreply.github.com>
2021-11-09 20:35:38 -0600

    Jake's Coding Club Challenge
    I'm working on this one...

---
+++ Jake's Problem.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Jake's challenge
+
+# In[20]:
+
+
+def star_num(input('Choose a number')):
+    num_list = []
+    num_list.append.input()
+
+for num in num_list: 
+    print('*')
+
+star_num()
+
+
+# In[25]:
+
+
+values = ['a','b','c','d','5']
+for value in values: 
+    print(value)
+
+
+# In[26]:
+
+
+index = 0
+for value in values: 
+    print(index, value)
+    index +=1
+    
+
+
+# In[27]:
+
+
+index = 0
+for value in values:
+    print('*')
+    index += 1
+
+
+# In[28]:
+
+
+for index in range(len(values)):
+    value = values[index]
+    print(index,value)
+
+
+# In[30]:
+
+
+def my_enumerate(sequence, start = 0):
+    n = start
+    for elem in sequence:
+        yield n, elem
+        n+=1
+ 
+
+
+# In[32]:
+
+
+seasons = ['spring','summer','fall','winter']
+
+my_enumerate(seasons)
+
+
+# In[33]:
+
+
+list(my_enumerate(seasons))
+
+
+# In[52]:
+
+
+def num_sub():
+    num = input('enter a number')
+    while int('num')
+        print('*')
+
+
+# In[53]:
+
+
+num_sub
+
+
+# In[54]:
+
+
+num_sub()
+
+
+# In[ ]:
+
+
+
+
